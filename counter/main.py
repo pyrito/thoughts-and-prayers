4e52e9a0354f9384ba49f3484a74a1946456d0ef
ribsthakkar <rishabh.thakkar@gmail.com>
2018-05-27 18:59:28 -0500

    Reformatted code and added some comments

--- counter/main.py
+++ counter/main.py
@@ -5,18 +5,24 @@ import datetime
 import sqlite3
 import os
 
-api = accessAPI.get_api()
-if not os.path.isfile('shootings.db'):
-    conn = sqlite3.connect('shootings.db')
-    c = conn.cursor()
-    c.execute("""CREATE TABLE events (
-            shooting_name text,
-            tps integer,
-            dos date
-            )  """)
-    c.execute("INSERT INTO events VALUES(?, ?, ?)", ("Noblesville", 0, datetime.datetime(2018,5,25)))
-    conn.commit()
-pc = prayersCounter.prayersCounter()
-search = searchAPI.searchAPI(api, pc)
-search.countPrayers()
-search.countPrayers(True)
+# Setup shooting database with inaugural shooting event if file not detected
+def setup_db():
+    if not os.path.isfile('shootings.db'):
+        conn = sqlite3.connect('shootings.db')
+        c = conn.cursor()
+        c.execute("""CREATE TABLE events (
+                shooting_name text,
+                tps integer,
+                dos date
+                )  """)
+        c.execute("INSERT INTO events VALUES(?, ?, ?)", ("Noblesville", 0, datetime.datetime(2018, 5, 25)))
+        conn.commit()
+
+# Begin running the tweet scraping process and updating database values
+def run_main():
+    api = accessAPI.get_api()
+    setup_db()
+    pc = prayersCounter.prayers_counter()
+    search = searchAPI.search_API(api, pc)
+    search.count_prayers()
+    search.count_prayers(True)
