321f46c8ac31366abf5eed35766bdd78b9642ddd
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-05-27 19:03:38 -0500

    nah

--- counter/main.py
+++ counter/main.py
@@ -5,18 +5,19 @@ import datetime
 import sqlite3
 import os
 
-api = accessAPI.get_api()
-if not os.path.isfile('shootings.db'):
-    conn = sqlite3.connect('shootings.db')
-    c = conn.cursor()
-    c.execute("""CREATE TABLE events (
-            shooting_name text,
-            tps integer,
-            dos date
-            )  """)
-    c.execute("INSERT INTO events VALUES(?, ?, ?)", ("Noblesville", 0, datetime.datetime(2018,5,25)))
-    conn.commit()
-pc = prayersCounter.prayersCounter()
-search = searchAPI.searchAPI(api, pc)
-search.countPrayers()
-search.countPrayers(True)
+def main_execute():
+	api = accessAPI.get_api()
+	if not os.path.isfile('shootings.db'):
+	    conn = sqlite3.connect('shootings.db')
+	    c = conn.cursor()
+	    c.execute("""CREATE TABLE events (
+	            shooting_name text,
+	            tps integer,
+	            dos date
+	            )  """)
+	    c.execute("INSERT INTO events VALUES(?, ?, ?)", ("Noblesville", 0, datetime.datetime(2018,5,25)))
+	    conn.commit()
+	pc = prayersCounter.prayersCounter()
+	search = searchAPI.searchAPI(api, pc)
+	search.countPrayers()
+	search.countPrayers(True)
