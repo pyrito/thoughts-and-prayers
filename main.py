45a9f07a169ca548bb068e4c02682030a1b3cbae
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-05-28 00:59:01 -0500

    made a few minor changes, reading from the database now, need to work on websockets

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 from counter.main import run_main
 import thread
+import sqlite3 as sql
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
 	thread.start_new_thread(run_main, ())
-	return render_template('index.html')
+	con = sql.connect("shootings.db")
+	cur = con.cursor()
+	cur.execute('SELECT tps FROM events WHERE shooting_name="Noblesville"')
+	num = cur.fetchone()[0]
+
+	return render_template('index.html', number = num)
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
