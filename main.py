eb6998505c691e1a2cd2943d896ee979ba581838
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-05-28 23:10:21 -0500

    got the websocket stuff to work, have to make sure to make only one thread

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 from counter.main import run_main
 import thread
+from time import sleep
+from threading import Thread, Event
 import sqlite3 as sql
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+
+#turn the flask into a socket app
+socketio = SocketIO(app)
+
+thread_update = Thread()
+thread_stop_event = Event()
+
+class UpdateThread(Thread):
+	def __init__(self):
+		self.delay = 1
+		super(UpdateThread, self).__init__()
+
+	def updater(self):
+		print("working")
+		con = sql.connect("shootings.db")
+		cur = con.cursor()
+		while not thread_stop_event.isSet():
+			cur.execute('SELECT tps FROM events WHERE shooting_name="Noblesville"')
+			num = cur.fetchone()[0]
+			socketio.emit('newnumber', {'number' : num}, namespace='/test')
+			sleep(self.delay)
+
+	def run(self):
+		self.updater()
 
 @app.route('/')
 def main():
 	thread.start_new_thread(run_main, ())
-	con = sql.connect("shootings.db")
-	cur = con.cursor()
-	cur.execute('SELECT tps FROM events WHERE shooting_name="Noblesville"')
-	num = cur.fetchone()[0]
+	return render_template('index.html')
 
-	return render_template('index.html', number = num)
+@socketio.on('connect', namespace='/test')
+def test_connect():
+	global thread_update
+	print('connected')
+
+	if not thread_update.isAlive():
+		thread_update = UpdateThread()
+		thread_update.start()
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    socketio.run(app)
