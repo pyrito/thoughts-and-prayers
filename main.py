12e20396b6df7c645e2a1cc2274927b443fe0041
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-05-29 13:06:45 -0500

    added some comments here and there

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 
 
-#turn the flask into a socket app
+#Turn the flask into a socket app
 socketio = SocketIO(app)
 
+#We have the update thread and the event that says to stop
 thread_update = Thread()
 thread_stop_event = Event()
 thread.start_new_thread(run_main, ())
 
+#Created a new Thread class that is specific to the Update Thread for the counter
 class UpdateThread(Thread):
 	def __init__(self):
 		self.delay = 1
@@ -25,11 +27,16 @@ class UpdateThread(Thread):
 
 	def updater(self):
 		print("working")
+		#Connect to the SQL database
 		con = sql.connect("shootings.db")
 		cur = con.cursor()
+
+		#As long as we don't get a signal to stop, we keep updating the thread
 		while not thread_stop_event.isSet():
+			#We select the corresponding data, for now, the database only has Noblesville, this can change
 			cur.execute('SELECT tps FROM events WHERE shooting_name="Noblesville"')
 			num = cur.fetchone()[0]
+			#We then send this to the listening socket
 			socketio.emit('newnumber', {'number' : num}, namespace='/test')
 			sleep(self.delay)
 
@@ -45,6 +52,7 @@ def test_connect():
 	global thread_update
 	print('connected')
 
+	#If the thread does not exist then we create the new thread and start it. This prevents multiple threads from coming up
 	if not thread_update.isAlive():
 		thread_update = UpdateThread()
 		thread_update.start()
